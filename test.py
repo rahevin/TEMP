5ec8b986c38f5bb291a0b502831484949bbf9b2f
= <rahevin@gmail.com>
2017-05-29 15:55:12 -0400

    Added square function

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ Created on Sat Oct 15 18:20:25 2016
 
 @author: WK
 """
+import math
 
 def add(x,y): 
     return x + y
@@ -16,3 +17,6 @@ def multiply(x, y):
 
 def divide(x, e):
 	return x / y 
+
+def square(x):
+    return math.sqrt(x)
