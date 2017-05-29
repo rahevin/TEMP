fbc36821bffdd4dd7757a0e283349276acad61b3
rahevin <rahevin@gmail.com>
2017-05-29 15:49:07 -0400

    added test.py to the repo

---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 15 18:20:25 2016
+
+@author: WK
+"""
+
+def add(x,y): 
+    return x + y
+
+def substract(x, y):
+    return x - y 
+
+def multiply(x, y):
+    return x * y 
+
+def divide(x, e):
+	return x / y 
